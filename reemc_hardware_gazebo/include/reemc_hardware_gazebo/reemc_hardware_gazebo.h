b9df0b4afb8a9e22cbd3c3058c99337399811bb1
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2014-10-20 18:45:35 +0200

    Reverting joint mode experimental stuff

--- reemc_hardware_gazebo/include/reemc_hardware_gazebo/reemc_hardware_gazebo.h
+++ reemc_hardware_gazebo/include/reemc_hardware_gazebo/reemc_hardware_gazebo.h
@@ -38,11 +38,9 @@
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/force_torque_sensor_interface.h>
 #include <hardware_interface/imu_sensor_interface.h>
-#include <hardware_interface/joint_mode_interface.h>
 #include <joint_limits_interface/joint_limits_interface.h>
 #include <pal_ros_control/pal_actuator_command_interface.h>
 #include <gazebo_ros_control/robot_hw_sim.h>
-#include <reemc_hardware_gazebo/mode_manager.h>
 
 #include <gazebo/physics/physics.hh>
 #include <gazebo/sensors/ImuSensor.hh>
@@ -66,7 +64,6 @@ public:
   void writeSim(ros::Time time, ros::Duration period);
 
 private:
-  typedef hardware_interface::JointCommandModes JntMode;
 
   // Raw data
   unsigned int n_dof_;
@@ -76,8 +73,6 @@ private:
   std::vector<double> jnt_eff_;
 
   std::vector<double> jnt_pos_cmd_;
-  std::vector<double> jnt_eff_cmd_;
-  std::vector<JntMode> jnt_mode_cmd_;
   std::vector<double> jnt_curr_limit_cmd_;
   std::vector<double> jnt_max_effort_;
 
@@ -99,8 +94,6 @@ private:
   // Hardware interface: joints
   hardware_interface::JointStateInterface    jnt_state_interface_;
   hardware_interface::PositionJointInterface jnt_pos_cmd_interface_;
-  hardware_interface::EffortJointInterface   jnt_eff_cmd_interface_;
-  hardware_interface::JointModeInterface     jnt_mode_cmd_interface_;
   hardware_interface::ActuatorStateInterface    act_state_interface_;
   hardware_interface::CurrentLimitActuatorInterface jnt_curr_limit_cmd_interface_;
 
@@ -111,16 +104,8 @@ private:
   // Joint limits interface
   joint_limits_interface::PositionJointSoftLimitsInterface jnt_limits_interface_;
 
-  // Guess what
-  std::map<std::string, hardware_interface::JointCommandModes> joint_modes_;
-
   // PID controllers
   std::vector<control_toolbox::Pid> pids_;
-
-  ModeManager mode_mgr_;
-  std::map<std::string, size_t> joint_index_lut_;
-
-  void sendPosition(size_t j, ros::Duration period);
 };
 
 }
